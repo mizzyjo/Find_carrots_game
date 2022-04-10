279c4ea53a85e469aff8bcc4be11a2f2eda5704e
mizzyjo <86835729+mizzyjo@users.noreply.github.com>
2022-04-10 21:53:54 +0900

    JS: Game field retry

--- main.js
+++ main.js
@@ -1,92 +1,33 @@
-// 당근과 벌레들을 각 다섯개씩 게임 필드 랜덤한 포지션에 나타나게 한다.
-const playBtn = document.querySelector('.game__button');
-const gameField = document.querySelector('.game__field');
-const fieldLeft = gameField.getBoundingClientRect().left;
-const fieldRight = gameField.getBoundingClientRect().right;
-const fieldTop = gameField.getBoundingClientRect().top;
-const fieldBottom = gameField.getBoundingClientRect().bottom;
-let carrotImgBox = [];
-let carrotCoorX = [];
-let carrotCoorY = [];
-let bugImgBox = [];
-let bugCoorX = [];
-let bugCoorY = [];
-let totalImgBox = [];
-console.log(`${fieldLeft}, ${fieldRight}, ${fieldTop}, ${fieldBottom}`);
-function init() {
-  for (let i = 0; i < 5; i++) {
-    // 1-1. 당근 랜덤 좌표 값 생성하기
-    let carrotX;
-    let carrotY;
-    while (1) {
-      carrotX = Math.floor(Math.random() * fieldLeft);
-      carrotY = Math.floor(Math.random() * fieldTop);
-      if (
-        fieldLeft < carrotX < fieldRight &&
-        fieldTop < carrotY < fieldBottom
-      ) {
-        console.log(`carrot x: ${carrotX}, y:${carrotY}`);
-        break;
-      }
-    }
-    // 1-2. 벌레 랜덤 좌표 값 생성하기
-    let bugX;
-    let bugY;
-    while (1) {
-      bugX = Math.floor(Math.random() * fieldLeft);
-      bugY = Math.floor(Math.random() * fieldTop);
-      if (fieldLeft < bugX < fieldRight && fieldTop < bugY < fieldBottom) {
-        console.log(`bug x: ${bugX}, y:${bugY}`);
-        break;
-      }
-    }
+const field = document.querySelector('.game__field');
+const fieldRect = field.getBoundingClientRect();
+const IMG_SIZE = 80;
 
-    // 2. `` 기호로 innerHTML 이용해 img 파일 랜덤 좌표에 심기
-    carrotImgBox[i] = `<img src="img/carrot.png"  alt="carrot" class="carrot">`;
-    carrotCoorX[i] = carrotX;
-    carrotCoorY[i] = carrotY;
+console.log(fieldRect);
 
-    bugImgBox[i] = `<img src="img/bug.png"  alt="carrot" class="bug">`;
-    bugCoorX[i] = bugX;
-    bugCoorY[i] = bugY;
-  }
-  console.log(carrotImgBox);
-  console.log(carrotCoorX);
-  console.log(carrotCoorY);
-  console.log('==============');
-  console.log(bugImgBox);
-  console.log(bugCoorX);
-  console.log(bugCoorY);
-  totalImgBox = carrotImgBox.concat(bugImgBox);
-  console.log(totalImgBox);
-  gameField.innerHTML = totalImgBox;
-  const carrots = document.querySelectorAll('.carrot');
-  for (let i = 0; i < 5; i++) {
-    carrots[
-      i
-    ].style.transform = `translate(${carrotCoorX[i]}px,${carrotCoorY[i]}px)`;
-  }
-  const bugs = document.querySelectorAll('.bug');
-  for (let i = 0; i < 5; i++) {
-    bugs[i].style.transform = `translate(${bugCoorX[i]}px,${bugCoorY[i]}px)`;
+function gameInit(classname, itemNumber, src) {
+  const x1 = fieldRect.x;
+  const x2 = x1 + fieldRect.width - IMG_SIZE;
+  const y1 = fieldRect.y;
+  const y2 = y1 + fieldRect.height - IMG_SIZE;
+  console.log(`${x1}, ${x2}, ${y1}, ${y2}`);
+  for (let i = 0; i < itemNumber; i++) {
+    const fixedX = randomNumber(x1, x2);
+    const fixedY = randomNumber(y1, y2);
+    console.log(`fixed: ${fixedX}, ${fixedY}`);
+    const item = document.createElement('img');
+    item.setAttribute('class', classname);
+    item.setAttribute('src', src);
+    item.style.position = 'absolute';
+    // item.style.transform = `translate(${fixedX}px,${fixedY}px)`;
+    item.style.left = `${fixedX}px`;
+    item.style.top = `${fixedY}px`;
+    field.appendChild(item);
   }
 }
 
-init();
+function randomNumber(min, max) {
+  return Math.random() * (max - min) + min;
+}
 
-// // 1. 랜덤 좌표 값 생성하기
-// let x;
-// let y;
-// while (1) {
-//   x = Math.floor(Math.random() * fieldLeft);
-//   y = Math.floor(Math.random() * fieldTop);
-//   if (fieldLeft < x < fieldRight && fieldTop < y < fieldBottom) {
-//     console.log(`x: ${x}, y:${y}`);
-//     break;
-//   }
-// }
-// // 2. `` 기호로 innerHTML 이용해 img 파일 랜덤 좌표에 심기
-// gameField.innerHTML = `<img src="img/carrot.png"  alt="carrot" class="carrot">`;
-// const carrot = document.querySelector('.carrot');
-// console.log(carrot);
-// carrot.style.transform = `translate(${x}px,${y}px)`;
+gameInit('carrot', 5, 'img/carrot.png');
+gameInit('bug', 5, 'img/bug.png');
